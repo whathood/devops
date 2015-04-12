c24f776f8bcf64404961668738559baff7464742
Jim Smiley <jsmiley@dunlap.home>
2015-04-12 10:43:03 -0400

    setups.sh - output msg changes

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# master setup script
+# runs
+#  * configure_ubuntu.sh to configure environment
+#  * chef_configure.sh - configures other stuff
+#  * copies custom .bash_aliases
+#  * setup_whathood.sh
+
 start_time=`date`
 
 SHARE_DIR="/vagrant"
 PROVISION_DIR="$SHARE_DIR/provision"
 
-echo "*       configuring ubuntu"
 source $PROVISION_DIR/configure_ubuntu.sh
-#
+
+source $PROVISION_DIR/chef_configure.sh
+
+echo "*       copying bash_alaises"
+cp $PROVISION_DIR/bash_aliases /home/vagrant/.bash_aliases
+chmod +r /home/vagrant/.bash_aliases
+
 echo "*       calling setup_whathood.sh"
-#
 source $PROVISION_DIR/setup_whathood.sh
 
-echo " "
 echo " "
 echo "setup complete"
 end_time=`date`
@@ -23,3 +33,4 @@ echo " "
 echo " "
 echo " "
 echo "visit http://localhost:8080 on your host machine to test"
+
