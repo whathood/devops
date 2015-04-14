00660a4f8b4c8f663a157a906ca70fc3f028f3af
Jim Smiley <jim.r.smiley@gmail.com>
2015-04-14 08:32:36 -0400

    setup_whathood.sh - changed code to ruby

--- provision/setup_whathood.sh
+++ provision/setup_whathood.sh
@@ -1,24 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env ruby
+
+# what this script does:
+#   pull down the git repository
+#   change the owner to vagrant
+#   import the data from the data repository
+#   run bin/deploy
+#   run fix_permissions
+#   touch the /var/log/whathood.lo
+#   copy over whathood.yaml
 
 APP_DIR='/var/www/whathood'
 
-chown -R vagrant.vagrant /usr/local
+`chown -R vagrant.vagrant /usr/local`
 
-echo "*      cloning Whathood"
-git clone https://github.com/jimRsmiley/whathood.git $APP_DIR > /dev/null 2>&1
-chown -R vagrant.vagrant $APP_DIR
-cd $APP_DIR/app
+puts "*      cloning Whathood"
+`git clone https://github.com/jimRsmiley/whathood.git #{APP_DIR} > /dev/null 2>&1`
+`chown -R vagrant.vagrant #{APP_DIR}`
+`cd $APP_DIR/app`
 
 # do NOT run as vagrant
-echo "*      importing data"
-cd $APP_DIR
-sudo -u vagrant $APP_DIR/bin/import_db
+puts "*      importing data"
+`cd #{APP_DIR}`
+`sudo -u vagrant #{APP_DIR}/bin/import_db`
 
-echo "*      deploying Whathood"
-cd $APP_DIR
-sudo -u vagrant $APP_DIR/bin/deploy
+puts "*      deploying Whathood"
+`cd #{APP_DIR}`
+`sudo -u vagrant #{APP_DIR}/bin/deploy`
 
-sudo -u vagrant $APP_DIR/dev-bin/fix_permissions
+`sudo -u vagrant #{APP_DIR}/dev-bin/fix_permissions`
 
-sudo touch /var/log/whathood.log
-sudo chown vagrant.vagrant /var/log/whathood.log
+`sudo touch /var/log/whathood.log`
+`sudo chown vagrant.vagrant /var/log/whathood.log`
+
+
