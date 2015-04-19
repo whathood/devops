12512edb2da3cbfc9118016407bf7fb5816f62ec
Jim Smiley <jim.r.smiley@gmail.com>
2015-04-19 10:06:47 -0400

    setup_whathood.rb - put everything into methods

--- provision/setup_whathood.rb
+++ provision/setup_whathood.rb
@@ -9,29 +9,42 @@
 #   touch the /var/log/whathood.lo
 #   copy over whathood.yaml
 
-APP_DIR='/var/www/whathood'
-PROVISION_DIR='/vagrant/provision'
-CONFIG_DIR="#{PROVISION_DIR}/config"
+APP_DIR       = '/var/www/whathood'
+PROVISION_DIR = '/vagrant/provision'
+CONFIG_DIR    = "#{PROVISION_DIR}/config"
 
 `chown -R vagrant.vagrant /usr/local`
 
-puts "*      cloning Whathood"
-`git clone https://github.com/jimRsmiley/whathood.git #{APP_DIR} > /dev/null 2>&1`
-`chown -R vagrant.vagrant #{APP_DIR}`
-Dir.chdir APP_DIR
+get_whathood_src(APP_DIR)
+get_db_data(APP_DIR)
+setup_whathood_yaml(APP_DIR,CONFIG_DIR)
+fix_file_permissions(APP_DIR)
 
-# do NOT run as vagrant
-puts "*      importing data"
-`sudo -u vagrant #{APP_DIR}/bin/import_db`
 
-`sudo touch /var/log/whathood.log`
-`sudo chown vagrant.vagrant /var/log/whathood.log`
+BEGIN {
 
-`cp #{CONFIG_DIR}/whathood.yaml #{APP_DIR}`
+    def get_whathood_src(app_dir)
+        puts "*      cloning whathood source"
+        `git clone https://github.com/jimRsmiley/whathood.git #{app_dir} > /dev/null 2>&1`
+        `git checkout wip/transfer`
+        `chown -R vagrant.vagrant #{app_dir}`
 
-puts "*      deploying Whathood"
-`cd #{APP_DIR}`
-`sudo -u vagrant #{APP_DIR}/bin/deploy`
+        puts "*      deploying Whathood"
+        Dir.chdir #{app_dir}
+        `sudo -u vagrant #{app_dir}/bin/deploy --no-prompt`
+    end
 
-`sudo -u vagrant #{APP_DIR}/dev-bin/fix_permissions`
+    def fix_file_permissions(app_dir)
+        `sudo -u vagrant #{app_dir}/dev-bin/fix_permissions`
+    end
 
+    def setup_whathood_yaml(app_dir,config_dir)
+        `cp #{config_dir}/whathood.yaml #{app_dir}`
+    end
+
+    def get_db_data(app_dir)
+        Dir.chdir app_dir
+        puts "*      importing data"
+        `sudo -u vagrant #{app_dir}/bin/import_db`
+    end
+}
