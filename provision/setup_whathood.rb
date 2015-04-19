c857b9f35d7cb06217ddfe9ad02aedd5c505283b
Jim Smiley <jim.r.smiley@gmail.com>
2015-04-19 11:20:42 -0400

    fixed bug where scripts could not chown src dir

--- provision/setup_whathood.rb
+++ provision/setup_whathood.rb
@@ -9,42 +9,76 @@
 #   touch the /var/log/whathood.lo
 #   copy over whathood.yaml
 
-APP_DIR       = '/var/www/whathood'
+require "fileutils"
+require "net/https"
+require "uri"
+
+APP_URL       = 'http://localhost:8080'
+SRC_DIR       = '/var/www/whathood'
 PROVISION_DIR = '/vagrant/provision'
 CONFIG_DIR    = "#{PROVISION_DIR}/config"
 
 `chown -R vagrant.vagrant /usr/local`
 
-get_whathood_src(APP_DIR)
-get_db_data(APP_DIR)
-setup_whathood_yaml(APP_DIR,CONFIG_DIR)
-fix_file_permissions(APP_DIR)
-
+get_whathood_src(SRC_DIR)
+run_whathood_deploy(SRC_DIR)
+get_db_data(SRC_DIR)
+setup_whathood_yaml(SRC_DIR,CONFIG_DIR)
+fix_file_permissions(SRC_DIR)
+test_live_site(APP_URL)
 
 BEGIN {
 
-    def get_whathood_src(app_dir)
+    def test_live_site(app_url)
+        puts "sleeping for 5 seconds....."
+        sleep 5
+        puts "checking for site index"
+        result = `wget http://localhost:80`
+        puts result
+
+        uri = URI.parse(app_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        res = http.request(request)
+
+        puts "res.code: #{res.code}"
+        if res.code != 200
+            puts "WARNING:\n\n**********\n\n\npolling #{app_url} did not return a 200 code"
+        end
+    end
+
+    def get_whathood_src(src_dir)
+        unless src_dir
+            abort "vagrant:setup_whathood.rb: src_dir must be defined"
+        end
+
         puts "*      cloning whathood source"
-        `git clone https://github.com/jimRsmiley/whathood.git #{app_dir} > /dev/null 2>&1`
+        `git clone https://github.com/jimRsmiley/whathood.git #{src_dir} > /dev/null 2>&1`
         `git checkout wip/transfer`
-        `chown -R vagrant.vagrant #{app_dir}`
+        FileUtils.chown_R "vagrant", "vagrant", src_dir
 
+    end
+
+    def run_whathood_deploy(src_dir)
         puts "*      deploying Whathood"
-        Dir.chdir #{app_dir}
-        `sudo -u vagrant #{app_dir}/bin/deploy --no-prompt`
+        Dir.chdir src_dir
+        `sudo #{src_dir}/bin/deploy --no-prompt`
     end
 
-    def fix_file_permissions(app_dir)
-        `sudo -u vagrant #{app_dir}/dev-bin/fix_permissions`
+    # should be run as root, not vagrant
+    def fix_file_permissions(src_dir)
+        `sudo #{src_dir}/dev-bin/fix_permissions`
     end
 
-    def setup_whathood_yaml(app_dir,config_dir)
-        `cp #{config_dir}/whathood.yaml #{app_dir}`
+    def setup_whathood_yaml(src_dir,config_dir)
+        `cp #{config_dir}/whathood.yaml #{src_dir}`
     end
 
-    def get_db_data(app_dir)
-        Dir.chdir app_dir
+    def get_db_data(src_dir)
+        Dir.chdir src_dir
         puts "*      importing data"
-        `sudo -u vagrant #{app_dir}/bin/import_db`
+        `sudo -u vagrant #{src_dir}/bin/import_db`
     end
 }
