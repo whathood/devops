5c23bcc541cdf4ab36a73315ce541298f77c6a52
Jim Smiley <jim.r.smiley@gmail.com>
2015-08-06 14:49:09 -0400

    update finished message

--- provision/finished_provision.sh
+++ provision/finished_provision.sh
@@ -7,5 +7,8 @@ puts " "
 puts " "
 puts "visit #{site} on your host machine to test"
 puts " "
-puts 'log into the machine with `vagrant ssh -c "screen -R"`'
-puts "logs will be in window 0"
+puts '* log into the machine with `vagrant ssh -c "screen -R"`'
+puts "    * logs will be in window 0"
+puts " "
+puts '* you can also edit the source code under the directory `whathood/src`'
+puts '* logs will be under `whathood/logs`'
