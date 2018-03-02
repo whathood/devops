9ae1209128a76986dd33c02b500b98565851e762
Jim Smiley <skiver.kelvin@gmail.com>
2018-03-02 14:07:11 -0500

    removed stale files

---
+++ docker/postgres/provisioning_files/install-pgrouting.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set +x
+
+# taken from https://github.com/pgRouting/pgrouting/wiki/Download%3A-Debian-and-Ubuntu
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+sudo apt-get -y install wget ca-certificates
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+
+sudo apt-get -y install postgresql-9.3-pgrouting
