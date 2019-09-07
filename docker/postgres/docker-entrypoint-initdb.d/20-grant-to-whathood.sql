515fc466dfcdb82b3b61b39e47353ba51d75d9d2
Jim Smiley <skiver.kelvin@gmail.com>
2019-09-07 17:30:53 -0400

    do it right and revoke from public

--- docker/postgres/docker-entrypoint-initdb.d/20-grant-to-whathood.sql
+++ docker/postgres/docker-entrypoint-initdb.d/20-grant-to-whathood.sql
@@ -1,5 +1,16 @@
 \connect whathood;
 
+-- why revoke? https://dba.stackexchange.com/questions/33943/granting-access-to-all-tables-for-a-user
+
+REVOKE CONNECT ON DATABASE whathood FROM PUBLIC;
+GRANT CONNECT
+ON DATABASE whathood
+TO whathood;
+
+REVOKE ALL
+ON ALL TABLES IN SCHEMA public 
+FROM PUBLIC;
+
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
 TO whathood;
